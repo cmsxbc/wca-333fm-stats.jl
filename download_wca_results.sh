401221478a1edffb0cc02afcdea74c245aefe38c
SuperDJY <cmsxbc@users.noreply.github.com>
2025-12-23 03:00:51 +0800

    Feat/support v2 (#1)
    * feat: notify v2 is not ready, and download v1 now
    
    * feat(v2): WIP
    
    * feat(v2): basic compatible

--- download_wca_results.sh
+++ download_wca_results.sh
@@ -2,19 +2,26 @@
 
 set -ex
 
+version="v2"
 WCA_PUB_URL="https://www.worldcubeassociation.org/export/results"
+WCA_PERMA_URL="https://www.worldcubeassociation.org/export/results/${version}/tsv"
 
 [[ -d "$1" ]] || (echo "$1 is not a dir" >&2 && exit 1)
 
 workdir="$1/$(date +%Y/%m/%d)"
 mkdir -p "$workdir"
-pub_html_file="$workdir/results.html"
+# pub_html_file="$workdir/results.html"
+# 
+# if [[ ! -e "$pub_html_file" ]];then
+#     curl -sSL "$WCA_PUB_URL" -o "$pub_html_file"
+# fi
 
-if [[ ! -e "$pub_html_file" ]];then
-    curl -sSL "$WCA_PUB_URL" -o "$pub_html_file"
+link_file="$workdir/permalink.$version.head.txt"
+if [[ ! -e "$link_file" ]];then
+    curl -I "$WCA_PERMA_URL" -o "$link_file"
 fi
 
-download_url="$(grep -Eo 'https://[^"]+.tsv.zip' "$pub_html_file")"
+download_url="$(grep -Eo 'https://[^"]+.tsv.zip' "$link_file")"
 
 [[ -z "$download_url" ]] && echo "cannot retrieve download url from html" && exit 1
 
@@ -22,7 +29,7 @@ zip_file_name="$(basename "$download_url")"
 
 [[ "$zip_file_name" =~ .tsv.zip ]] || (echo "invalid tsv zip file: $zip_file_name" && exit 1)
 
-# [[ "$zip_file_name" =~ $(date +%Y%m%d)T ]] || (echo "not today's tsv zip file: $zip_file_name" && exit 1)
+[[ "$zip_file_name" =~ $(date +%Y%m%d)T ]] || (echo "not today's tsv zip file: $zip_file_name" && exit 1)
 
 zip_file_path="$workdir/$zip_file_name"
 
